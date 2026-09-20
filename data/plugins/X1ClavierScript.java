7e35dc23a454f46728581607ae193e3339861ea8
Anjasnare <tommy.coquart@orange.fr>
2026-09-20 13:38:15 +0200

    Update X1ClavierScript.java

--- data/plugins/X1ClavierScript.java
+++ data/plugins/X1ClavierScript.java
@@ -3,61 +3,38 @@ package data.plugins;
 import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignUIAPI;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
-import com.fs.starfarer.api.campaign.InteractionDialogPlugin;
-import com.fs.starfarer.api.campaign.SectorEntityToken;
-import com.fs.starfarer.api.campaign.OptionPanelAPI;
 import org.lwjgl.input.Keyboard;
 
 public class X1ClavierScript implements EveryFrameScript {
     
+    private boolean isPressed = false;
+    
+    @Override
     public boolean isDone() { return false; }
+    
+    @Override
     public boolean runWhilePaused() { return true; }
     
+    @Override
     public void advance(float amount) {
         if (Global.getSector() == null) return;
         CampaignUIAPI ui = Global.getSector().getCampaignUI();
-        if (ui == null || ui.getCurrentInteractionDialog() == null) return;
+        if (ui == null) return;
         
-        InteractionDialogAPI dialog = ui.getCurrentInteractionDialog();
-        InteractionDialogPlugin originalPlugin = dialog.getPlugin();
-        if (originalPlugin == null || originalPlugin instanceof X1TerminalMenu) return;
-
-        OptionPanelAPI options = dialog.getOptionPanel();
-        if (options != null) {
-            String pluginClass = originalPlugin.getClass().getName().toLowerCase();
+        // CONFINEMENT TOTAL : N'ouvrir que si le joueur est en vol libre dans l'espace.
+        // Si ui.getCurrentInteractionDialog() est nul, aucun panneau de station ou de flotte n'est ouvert.
+        if (ui.getCurrentInteractionDialog() == null) {
             
-            if (pluginClass.contains("barcmd") || pluginClass.contains("combat") || 
-                pluginClass.contains("core_refit") || pluginClass.contains("fleet") ||
-                pluginClass.contains("contact") || pluginClass.contains("intel") || 
-                pluginClass.contains("officer") || pluginClass.contains("person")) {
-                if (options.hasOption("TERMINAL_EXCHANGE_REFIT")) {
-                    options.removeOption("TERMINAL_EXCHANGE_REFIT");
+            if (Keyboard.isKeyDown(Keyboard.KEY_K)) {
+                if (!isPressed) {
+                    isPressed = true;
+                    
+                    // Instanciation de votre terminal sous forme de fenêtre volante autonome
+                    X1TerminalMenu menu = new X1TerminalMenu(null);
+                    ui.showInteractionDialog(menu, Global.getSector().getPlayerFleet());
                 }
-                return; 
-            }
-
-            SectorEntityToken cible = dialog.getInteractionTarget();
-            if (cible != null && cible.getMarket() != null) {
-                if (dialog.getInteractionTarget().getActivePerson() != null) {
-                    if (options.hasOption("TERMINAL_EXCHANGE_REFIT")) {
-                        options.removeOption("TERMINAL_EXCHANGE_REFIT");
-                    }
-                    return;
-                }
-
-                boolean fr = X1Localisation.estEnFrancais();
-                String boutonTexte = fr ? "Pour accéder au Terminal ExchangeRefit, appuyez sur [X]" : "For access to ExchangeRefit Terminal, press [X]";
-
-                if (!options.hasOption("TERMINAL_EXCHANGE_REFIT")) {
-                    options.addOption(boutonTexte, "TERMINAL_EXCHANGE_REFIT");
-                }
-
-                if (Keyboard.isKeyDown(Keyboard.KEY_X)) {
-                    X1TerminalMenu menu = new X1TerminalMenu(originalPlugin);
-                    dialog.setPlugin(menu);
-                    menu.init(dialog);
-                }
+            } else {
+                isPressed = false;
             }
         }
     }
